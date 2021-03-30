3d5b580ffc4fda1e6bab73fe845174cd16df2d7f
Philipsd21 <46075925+Philipsd21@users.noreply.github.com>
2021-03-30 14:02:21 +0100

    Updated the correlation processing

--- Python/Correlation.py
+++ Python/Correlation.py
@@ -1,14 +1,22 @@
 import numpy as np
 import pandas as pd
+import csv
 
 # https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.corr.html#pandas.Series.corr
 
 def histogram_intersection(a,b):
     v = np.minimum(a,b).sum()
     return v
-
-Variance = pd.Series([.223,.324,.41,.51231])
-Tweets = pd.Series([2,32,70,43])
+readfile = ""
+reader = csv.reader(readfile)
+VarianceData = []
+TweetsData = []
+for line in reader:
+    lineData = line.split(",")
+    VarianceData.append((float)(lineData[0]))
+    TweetsData.append((float)(lineData[1]))
+Variance = pd.Series(VarianceData)
+Tweets = pd.Series(TweetsData)
 print("Test Correlation:",Variance.corr(Tweets,method=histogram_intersection))
 
 

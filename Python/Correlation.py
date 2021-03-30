e066ec1e1be637edcfda2842a29b10777eb6664d
Philipsd21 <46075925+Philipsd21@users.noreply.github.com>
2021-03-30 14:09:40 +0100

    Update Correlation.py

--- Python/Correlation.py
+++ Python/Correlation.py
@@ -1,22 +1,14 @@
-import numpy as np
+# importing pandas as pd
 import pandas as pd
-import csv
-
-# https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.Series.corr.html#pandas.Series.corr
-
-def histogram_intersection(a,b):
-    v = np.minimum(a,b).sum()
-    return v
-readfile = ""
-reader = csv.reader(readfile)
-VarianceData = []
-TweetsData = []
-for line in reader:
-    lineData = line.split(",")
-    VarianceData.append((float)(lineData[0]))
-    TweetsData.append((float)(lineData[1]))
-Variance = pd.Series(VarianceData)
-Tweets = pd.Series(TweetsData)
-print("Test Correlation:",Variance.corr(Tweets,method=histogram_intersection))
-
-
+  
+# Making data frame from the csv file
+readFile = ""
+df = pd.read_csv(readFile)
+  
+# Printing the first 10 rows of the data frame for visualization
+df[:10]
+output = df.corr(method ='kendall')
+forGivenStock = output[0][2]
+forBaseline = output[1][2]
+difference = forGivenStock - forBaseline
+print(forGivenStock)

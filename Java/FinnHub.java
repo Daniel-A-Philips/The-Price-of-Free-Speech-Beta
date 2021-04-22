b709b37e3bb3f0da477eb9add77f8c6c64784f93
Daniel Philips <daniel_philips@asl.org>
2021-04-22 23:35:31 +0100

    Update

--- Java/FinnHub.java
+++ Java/FinnHub.java
@@ -10,7 +10,9 @@ public class FinnHub {
     public String Ticker;
     public String Format = "csv";
     public ArrayList<String> Lines = new ArrayList<String>();
-    public LocalDate[] Dates;
+    public ArrayList<LocalDate[]> Dates = new ArrayList<LocalDate[]>();
+    private String[] tempMonthArray = new String[] {"" ,"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
+
     String[] start_end = new String[2];
 
     FinnHub(String start, String end, String Ticker, String Interval){
@@ -18,54 +20,81 @@ public class FinnHub {
         start_end[1] = end;
         this.Ticker = Ticker;
         this.Interval = Interval;
-        run();
+        betaRun();
     }
 
     private void run() {
-        try{
-            long ut1 = Instant.now().getEpochSecond();
-            System.out.println(ut1);
+        /*try{
             long[] times = getDates();
             System.out.println(times[0] + ", "+ times[1]);
-            URL url = new URL("https://finnhub.io/api/v1/stock/candle?symbol=" + Ticker + "&resolution=" + Interval + "&from=" + times[0] + "&to=" + times[1] + "&format=" + Format + "&token=c1vv82l37jkoemkedus0");
+            urlConnect(times[0],times[1]);
+            writeToCSV();
+        } catch(Exception e){ System.out.println(e + " occured on line " + e.getStackTrace()[0].getLineNumber() + " in FinnHub.java");}*/
+    }
+
+    private void betaRun(){
+        try{
+            ArrayList<long[]> times = getDates();
+            for(long[] time : times){
+                System.out.println(time[0] + ", "+ time[1]);
+                urlConnect(time[0],time[1]);
+            }
+            writeToCSV();
+        } catch(Exception e){ System.out.println(e + " occured on line " + e.getStackTrace()[0].getLineNumber() + " in FinnHub.java");}
+    }
+
+    private void urlConnect(long start, long end){
+        try{
+            URL url = new URL("https://finnhub.io/api/v1/stock/candle?symbol=" + Ticker + "&resolution=" + Interval + "&from=" + start + "&to=" + end + "&format=" + Format + "&token=c1vv82l37jkoemkedus0");
             InputStream in = url.openStream();
             BufferedReader reader = new BufferedReader(new InputStreamReader(in));
             String line;
+            ArrayList<String> temp = new ArrayList<String>();
             while((line = reader.readLine()) != null) {
                 //System.out.println(line);
+                if(Lines.contains(line)) continue;
+                temp.add(line);
                 Lines.add(line);
             }
-            writeToCSV();
-        } catch(Exception e){ System.out.println(e + "occured on line" + e.getStackTrace() + "in FinnHub.java");}
+            temp.addAll(Lines);
+            Lines = temp;
+        } catch(Exception e){ System.out.println(e + " occured on line " + e.getStackTrace()[0].getLineNumber() + " in FinnHub.java");}
     }
 
-    private long[] getDates(){
+    private ArrayList<long[]> getDates(){
         String a = start_end[0];
         String b = start_end[1];
-        String[] tempMonthArray = new String[] {"" ,"January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"};
         ArrayList<String> months = new ArrayList<String>(Arrays.asList(tempMonthArray));
+        ArrayList<long[]> holder = new ArrayList<long[]>();
+        //String[] start_end_months = new String[]{a.replaceAll("[0-9]", "").replaceAll(" ", ""), b.replaceAll("[0-9]", "").replaceAll(" ", "")};
         if(a.equals(b)){ //If only one month is selected
             int year = Integer.parseInt(a.substring(a.length()-4,a.length()));
+            System.out.println(year);
             int month = Integer.parseInt(a.substring(0,a.length()-3));
+            System.out.println("year : " + year + ", month : " + month);
             YearMonth temp = YearMonth.of(year,month);
-            LocalDate strtDate = LocalDate.of(year,month,1);
-            LocalDate endDate = LocalDate.of(year,month,temp.lengthOfMonth());
-            Dates = new LocalDate[]{strtDate,endDate};
-            return new long[]{toUnix(strtDate),toUnix(endDate)};
+            LocalDate date1 = LocalDate.of(year,month,1);
+            LocalDate date2 = LocalDate.of(year,month,temp.lengthOfMonth());
+            Dates.add(new LocalDate[]{date1,date2});
+            holder.add(new long[]{toUnix(date1),toUnix(date2)});
+            return holder;
         }
         int year1 = Integer.parseInt(a.substring(a.length()-4,a.length()));
         int year2 = Integer.parseInt(b.substring(b.length()-4,b.length()));
-        System.out.println("year1 " + year1);
-        System.out.println("Month1 " + a.substring(0,a.length()-5));
         int month1 = months.indexOf(a.substring(0,a.length()-5));
         int month2 = months.indexOf(b.substring(0,b.length()-5));
-        YearMonth temp = YearMonth.of(year2,month2);
-        LocalDate date1 = LocalDate.of(year1,month1,1);
-        System.out.println(date1);
-        LocalDate date2 = LocalDate.of(year2,month2,temp.lengthOfMonth());
-        System.out.println("Year 2 " + year2);
-        Dates = new LocalDate[]{date1,date2};
-        return new long[]{toUnix(date1),toUnix(date2)};
+        int timesToRun = month2-month1;
+        for(int i = 0; i < timesToRun; i++){
+            System.out.println(month2-1-i);
+            LocalDate date1 = LocalDate.of(year1,month2-1-i,1);
+            System.out.println(date1);
+            YearMonth temp = YearMonth.of(year2,month2-i);
+            LocalDate date2 = LocalDate.of(year2,month2-i,temp.lengthOfMonth());
+            System.out.println(date2);
+            Dates.add(new LocalDate[]{date1,date2});
+            holder.add(new long[]{toUnix(date1),toUnix(date2)});
+        }
+        return holder;
     }
 
     private long toUnix(LocalDate date){ 
@@ -88,6 +117,7 @@ public class FinnHub {
                 long unix = Long.parseLong(line.substring(0,line.indexOf(",")));
                 Date date = new Date ();
                 date.setTime((long)unix*1000);
+                if(line.contains("1609459260")) System.out.println(line);
                 line = date + line.substring(line.indexOf(","),line.length()); //Line used to change UNIX to date and time format on csv file
             }
             writer.write(line + "\n");
